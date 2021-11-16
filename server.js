6e53e28fac6cd4047f6a10675b6ad61a207abae1
OMAR RAMON <omarmontes.879@gmail.com>
2021-11-16 18:52:48 -0500

    Servidor

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname+'/dist/centros-recreativos-ufps-frontend'));
-app.get('/',function(req,res){
+app.get('*',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/centros-recreativos-ufps-frontend/index.html'));
 });
 
